6816fecd7d867fa315504c22e1af649a421305a6
Sabke papa <jhasankalp2005@gmail.com>
2026-07-19 04:53:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 # The only two things you must fill in:
-EMAIL = "22f3000782@ds.study.iitm.ac.in."
+EMAIL = "22f3000782@ds.study.iitm.ac.in"
 AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjMwMDA3ODJAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDI2MzkxOCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDg2ODcxOH0.juJqpWXxmP3ysv_3vak6n6IjbqhPZHBbzoSM0gFJrvE"
 
 # Fixed — do not change
